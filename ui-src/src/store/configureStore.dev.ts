6dec5bdb0e00edcdebf95f1162de79c63272a435
Lisa <ldjetton21@gmail.com>
2019-04-25 12:22:12 -0500

    updates to make holofuel compatible

--- ui-src/src/store/configureStore.dev.ts
+++ ui-src/src/store/configureStore.dev.ts
@@ -5,19 +5,31 @@ import { routerMiddleware, routerActions } from 'connected-react-router';
 import { createLogger } from 'redux-logger';
 import createRootReducer from '../reducers/index';
 import * as transactionActions from '../actions/transactionActions';
+
+// ** Middleware for HC Rust Container Communication ** >> Reference Holochain-UI //
+// import { setPort } from '../utils/constants';
+// import { connect } from '@holochain/hc-web-client';  // '@holochain/hc-web-client'
+
 // ** Middleware for HC Rust Container Communication ** >> Reference Holochain-UI //
 import { holochainMiddleware } from '@holochain/hc-redux-middleware';
-import { connect } from '@holochain/hc-web-client';  // '@holochain/hc-web-client'
-import { setPort } from '../utils/constants'
+import * as holochainclient from '@holochain/hc-web-client';
+import * as hClient from '@holo-host/hclient';
+
+(async function findAgentID () {
+  const agent_id = await hClient.getCurrentAgentId();
+  console.log("YOUR agent_id === ", agent_id);
+})();
 
 // const url = 'ws:localhost:3000';
-const url = `ws:localhost:${setPort()}`;
-const hcWc = connect(url);
+// const url = `ws:localhost:${setPort()}`;
+// const hcWc = connect(url);
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
 
 const configureStore = (initialState?: any) => {
+  console.log("in it init")
+
   // Redux Configuration
   const middleware = [];
   const enhancers = [];
@@ -40,8 +52,31 @@ const configureStore = (initialState?: any) => {
   const router = routerMiddleware(history);
   middleware.push(router);
 
+  //*********************** Use this for intrceptr server ********************//
+  // const HOSTURL = 'ws://' + window.location.host;
+  const HOSTURL = 'http://holofuel6example.holohost.net'
+   console.log(" >>> HOST URL <<<<", HOSTURL);
+
+   const KV_STORE_HAPPHASH = "holofuel";
+   // const KV_STORE_HAPPHASH = "holofuel3_dna"
+
+   /***********************  CALLING `makeWebClient` WITH OPTIONALS **************************/
+   // If have a DNA-STRING, supplement the options/params with this: { url:URL, dnaHash:DNA })
+   // const HAPPHASH:any = 'Qmd3zeMA5S5YWQ4QAZ6JTBPEEAEJwGmoSxkYn6y2Pm4PNV';
+   hClient.makeWebClient(holochainclient, KV_STORE_HAPPHASH, { hostUrl:HOSTURL })
+   .then((holoClient:any) => holochainMiddleware(holoClient.connect()));
+
+   /***************************** WITHOUT OPTIONALS *****************************/
+   // If NO `hostUrl` optional is provided in `makeWebClient`, then the function should triggr the Resolver to locate a host & host-url/host-domain
+   // hClient.makeWebClient(holochainclient, KV_STORE_HAPPHASH)
+   // .then((holoClient:any) => holochainMiddleware(holoClient.connect()));
+
+  //******************* ********************************** ******************//
+
   // ** HC Rust Container Middleware ** >> Push HC middleware into middleware array //
-  middleware.push(holochainMiddleware(hcWc));
+  // middleware.push(holochainMiddleware(hcWc));
+
+  //******************* ********************************** ******************//
 
   // Redux DevTools Configuration
   const actionCreators = {
---
+++ ui-src/src/untyped-modules.d.ts
@@ -0,0 +1,5 @@
+declare module '@holo-host/hclient' {
+    var _a: any;
+    export = _a;
+}
+// declare module "another-untyped-module";
